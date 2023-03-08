4d28b277ca786a91abd7d897039cd96bcdb789f8
ziadNady <nadyziad25@gmail.com>
2023-03-08 07:38:52 +0200

    finished validatin and routes

---
+++ Routers/memberRouter.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const validateMW = require("../Core/Validations/validateMW");
+const controller = require("../Controllers/memberController");
+const validateMember = require("../Core/Validations/validateMember");
+const multerMW = require("../Core/Multer/multerMW");
+
+const router = express.Router();
+
+router("/members")
+                .get(controller.getAllMembers)
+                .post(
+                    multerMW,
+                    validateMember.validatePostArray,
+                    validateMW,
+                    controller.addMember
+                    )
+                .patch(
+                    multerMW,
+                    validateMember.validatePatchArray,
+                    validateMW,
+                    controller.updateMember
+                )
+                .delete(
+                    validateMember.validateId,
+                    validateMw,
+                    controller.deleteMember
+                );
+
+router.route("/employeed/:id")
+                            .get(
+                                validateMember.validateId,
+                                validateMW,
+                                controller.getEmployeeById
+                            );
+
+module.exports = router;
