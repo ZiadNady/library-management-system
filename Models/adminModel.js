29989485a7a16ad16f88fa4ee8d1c19d7e950259
Ayman Salah <ayman.1551.salah@gmail.com>
2023-03-10 06:52:53 +0200

    Admin Model

---
+++ Models/adminModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const extendSchema = require("mongoose-extend-schema");
+const autoIncrement = require("mongoose-auto-increment");
+const basicUserSchema = require("./userModel");
+autoIncrement.initialize(mongoose.connection);
+
+const adminSchema = extendSchema(basicUserSchema, {
+  hireDate: {
+    type: Date,
+    default: Date.now
+  },
+  salary: {
+    type: Number,
+    default: 3500
+  }
+});
+adminSchema.plugin(autoIncrement.plugin, "admin");
+mongoose.model("admin", adminSchema);
