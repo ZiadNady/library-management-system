8bf59950cd51bf56ad0ea467c7672a8e9265d484
ziadNady <nadyziad25@gmail.com>
2023-03-12 23:10:49 +0200

    re-add validation rules

--- Core/Validations/validateMember.js
+++ Core/Validations/validateMember.js
@@ -1,76 +1,29 @@
 const { body, param } = require("express-validator");
 
 exports.validatePostArray = [
-  body("firstName").isAlpha().withMessage("First name must be alphabetic"),
-  body("lastName").isAlpha().withMessage("Last name must be alphabetic"),
-  body("email").isEmail().withMessage("Email is not valid"),
-  body("gender")
-    .optional()
-    .isIn(["male", "female"])
-    .withMessage("Gender must be either male or female"),
-  body("birthDate")
-    .optional()
-    .isDate()
-    .withMessage("Birth date must be a valid date"),
-    
+    body("firstName").isAlpha().withMessage("First name must be alphabetic"),
+    body("lastName").isAlpha().withMessage("Last name must be alphabetic"),
+    body("email").isEmail().withMessage("Email is not valid"),
+    body("gender").optional().isIn(["male", "female"]).withMessage("Gender must be either male or female"),
+    body("birthDate").optional().isDate().withMessage("Birth date must be a valid date"),
+    body("address").isObject().withMessage("Address must be an object")
 ];
 
-exports.validatePatchArrayAdmin = [
-  body("id").isNumeric().withMessage("Id must be a number"),
-  body("firstName")
-    .optional()
-    .isAlpha()
-    .withMessage("First name must be alphabetic"),
-  body("lastName")
-    .optional()
-    .isAlpha()
-    .withMessage("Last name must be alphabetic"),
-  body("email").optional().isEmail().withMessage("Email is not valid"),
-  body("password")
-    .optional()
-    .isStrongPassword()
-    .withMessage("Password must be strong"),
-  body("gender")
-    .optional()
-    .isIn(["male", "female"])
-    .withMessage("Gender must be either male or female"),
-  body("birthDate")
-    .optional()
-    .isDate()
-    .withMessage("Birth date must be a valid date"),
-  body("salary").optional().isNumeric().withMessage("Salary must be a number"),
-  body("image").optional().isString().withMessage("Image must be a string"),
-];
 
-exports.validatePatchArrayEmployee = [
-  body("id").isNumeric().withMessage("Id must be a number"),
-  body("firstName")
-    .optional()
-    .isAlpha()
-    .withMessage("First name must be alphabetic"),
-  body("lastName")
-    .optional()
-    .isAlpha()
-    .withMessage("Last name must be alphabetic"),
-  body("password")
-    .optional()
-    .isStrongPassword()
-    .withMessage("Password must be strong"),
-  body("gender")
-    .optional()
-    .isIn(["male", "female"])
-    .withMessage("Gender must be either male or female"),
-  body("birthDate")
-    .optional()
-    .isDate()
-    .withMessage("Birth date must be a valid date"),
-  body("image").optional().isString().withMessage("Image must be a string"),
+exports.validatePatchArrayMember = [
+  body("_id").isNumeric().withMessage("Id must be a number"),
+  body("firstName").optional().isAlpha().withMessage("First name must be alphabetic"),
+  body("lastName").optional().isAlpha().withMessage("Last name must be alphabetic"),
+  body("password").optional().isStrongPassword().withMessage("Password must be strong"),
+  body("gender").optional().isIn(["male", "female"]).withMessage("Gender must be either male or female"),
+  body("birthDate").optional().isDate().withMessage("Birth date must be a valid date"),
+  body("address").optional().isObject().withMessage("Address must be an object")
 ];
 
 exports.validateId = [
-  body("id").isNumeric().withMessage("Employee id should be number"),
+  body("_id").isNumeric().withMessage("Employee id should be number"),
 ];
 
 exports.validateIdParam = [
-  param("id").isNumeric().withMessage("Employee id should be number"),
+  param("_id").isNumeric().withMessage("Employee id should be number"),
 ];
